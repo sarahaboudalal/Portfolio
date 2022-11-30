ff3498df9b012447125654ad81756a147db8986f
Sarah Abou Dalal <sarahaboudalal@gmail.com>
2022-11-30 19:57:00 +0200

    footer fixes

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,13 +2,13 @@ import { MdEmail } from "react-icons/md";
 
 export default function Footer() {
     return (
-      <div className="bg-greenish border-t-2 border-t-beige py-4 px-2 flex flex-row items-center justify-center">
-        <p className="text-center text-beige font-medium">Let's Get in Touch</p>{' '}
-        <a
-          href="mailto:sarahaboudalal@gmail.com"
-          className="mx-4 text-sm font-medium hover:text-beige text-whitish"
-        >
-          <MdEmail className="h-8 w-4" />
+      <div className="bg-greenish border-t-2 border-t-beige py-4 px-2 flex items-center justify-center">
+      <a
+        href="mailto:sarahaboudalal@gmail.com"
+        className="mx-4 text-sm font-medium hover:text-beige text-whitish"
+      >
+       Let's Get in Touch{' '}
+          <MdEmail className="h-8 w-4 inline" />
         </a>
       </div>
     );
