89116f42bd52de43eecb03240981b30602439c11
Sarah Abou Dalal <sarahaboudalal@gmail.com>
2022-11-30 15:35:05 +0200

    added logo

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -8,10 +8,16 @@ export default function Navbar() {
     <div>
       <nav className="bg-greenish">
         <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl px-4 md:px-6 py-6">
-          <span className="self-center hover:text-redish text-whitish text-3xl font-bold">
-            Sarah Abou Dalal
-          </span>
-
+          <div className="flex items-center">
+            <img
+              src="https://cdn.discordapp.com/attachments/1031834305703460906/1047504083579326536/Untitled-1-01.png"
+              className="mr-3 h-9"
+              alt="Flowbite Logo"
+            />
+            <span className="self-center hover:text-redish text-whitish text-3xl font-bold">
+              Sarah Abou Dalal
+            </span>
+          </div>
           <div className="flex items-center">
             <a
               href="https://github.com/sarahaboudalal"
