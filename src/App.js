878979384f8805ac3f9001937b46c08f843fbed7
Sarah Abou Dalal <sarahaboudalal@gmail.com>
2022-12-12 18:07:54 +0200

    added skills and about pages

--- src/App.js
+++ src/App.js
@@ -1,21 +1,19 @@
 import { Route, Routes } from 'react-router-dom';
-import About from './components/About/About';
 import Footer from './components/Footer/Footer';
 import HomePage from './pages/Home/HomePage';
-import Hero from './components/Hero/Hero';
 import Navbar from './components/Navbar/Navbar';
-import ProjectSection from './components/Projects/ProjectSection';
-// import Project from './components/Projects/Projects';
-// import PorjectSection from './components/Projects/ProjectSection';
-import Skills from './components/Skills/Skills';
 import './index.css';
+import SkillsPage from './pages/Skills/SkillsPage';
+import AboutPage from './pages/About/AboutPage';
 
 function App() {
   return (
     <div>
       <Navbar />
       <Routes>
-        <Route path='/' element={<HomePage/> } />
+        <Route path='/' element={<HomePage />} />
+        <Route path='skills' element={<SkillsPage />} />
+        <Route path='/about' element={ <AboutPage/>} />
       </Routes>
       <Footer />
       </div>
@@ -23,6 +21,3 @@ function App() {
     }
     
     export default App;
-    // <Routes>
-    //   <Route path='/' element={<HomePage/>} />
-    // </Routes>
