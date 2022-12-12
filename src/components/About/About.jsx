859682e1eb2862851dbe1bfa141caf72bb9d59d8
Sarah Abou Dalal <sarahaboudalal@gmail.com>
2022-12-12 18:52:44 +0200

    updated about me page

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,32 +1,42 @@
 export default function About() {
   return (
-    <div className="bg-beige py-8 md:px-2 px-4" id="about-section">
+    <div className="bg-whitish py-16 px-12" id="about-section">
       <h3 className="mb-10 text-center overline text-greenish text-4xl font-bold">
         About Me
       </h3>
-      <p className="px-6 text-lg my-1 font-medium text-night">
-        I'm a Front End Web Developer with basic knowledge in graphic designing.
-        I'm passionate about expanding my knowledge in the tech field and
-        solving problems. Well-versed in debugging and building responsive and
-        functional websites, with a strong skill set in effective communication
-        and creative thinking, and a background in teamwork and collaboration.
-      </p>
-      <p className="px-6 text-lg my-4 font-medium text-night">
-        As for my former education, I studied Nursing at the Lebanese University
-        and graduated in 2020. It took me 1 year after graduation to
-        realise that I don't want nursing as a career anymore, and that's how my
-        career switching journey began. I found myself so fund with coding and
-        tech, and I started looking for an oportunity to learn more. I finally
-        found myself applying to one of Re:Coded's Front-End bootcamps, and I
-        was one of the lucky people who were accepted there. In a period of 5
-        months, I jumped from someone who knows nothing about coding, to a
-        really confident Front End Web Developer who's even more ready to learn
-        new things and discover the world of tech more deeply.
-      </p>
-      <p className="px-6 text-lg my-1 font-medium text-night">
-        Outside the coding world, I love reading, photography, hiking, and
-        embroidering.
-      </p>
+      <div className="flex lg:flex-row flex-col">
+        <img
+          src="https://cdn.discordapp.com/attachments/1035620424366698537/1040399760936407140/20221111_005321.jpg"
+          alt="profile"
+          className="w-72 h-72 lg:self-start self-center md:inline hidden lg:my-0 my-6 rounded-md shadow-lg"
+        />
+        <div className="px-6 text-2xl my-1 font-medium text-night">
+          <p className="pb-3">
+          I'm a Front End Web Developer with basic knowledge in graphic
+          designing. I'm passionate about expanding my knowledge in the tech
+          field and solving problems. Well-versed in debugging and building
+          responsive and functional websites, with a strong skill set in
+          effective communication and creative thinking, and a background in
+          teamwork and collaboration.
+          </p>
+          <p>
+          As for my former education, I studied Nursing at the Lebanese
+          University and graduated in 2020. It took me 1 year after graduation
+          to realise that I don't want nursing as a career anymore, and that's
+          how my career switching journey began. I found myself so fund with
+          coding and tech, and I started looking for an oportunity to learn
+          more. Eventually I applied to one of Re:Coded's Front-End
+          bootcamps, and I was one of the lucky people who were accepted there.
+          In a period of 5 months, I jumped from someone who knew nothing about
+          coding, to a really confident Front End Web Developer who's even more
+          ready to learn new things and discover the world of tech more deeply.
+          </p>
+          <p className="pt-3">
+          Outside the coding world, I love reading, photography, hiking, and
+            embroidering.
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,28 +1,30 @@
 import { BsCodeSlash, BsCode } from 'react-icons/bs';
+import { Link } from 'react-router-dom';
 
 export default function Hero() {
   return (
-    <div className="bg-beige h-full py-4 md:px-0 px-4">
-      <div className="flex flex-col justify-center md:items-center text-start py-5">
-        <h3 className="text-greenish font-extrabold text-5xl py-3">
+    <div className="bg-whitish h-full py-4 md:px-0 px-4">
+      <div className="flex flex-col justify-center items-center py-5">
+        <h3 className="text-greenish font-extrabold text-5xl py-3 text-center">
           Welcome to My Portfolio
         </h3>
-        <h3 className="text-greenish font-bold text-3xl py-3">
-          I'm Sarah, a<BsCode className="inline text-greenish w-12" />
-          <p className="inline underline">Front End Web Developer</p>
-          <BsCodeSlash className="inline text-greenish w-12" />
+        <h3 className="text-greenish font-bold text-2xl py-3 text-center">
+          Hi! I'm Sarah Abou Dalal, a
+          <BsCode className="md:inline hidden text-greenish w-12" />
+          <p className="inline underline mx-2 md:mx-0">Front End Web Developer</p>
+          <BsCodeSlash className="md:inline hidden text-greenish w-12" />
         </h3>
-        <h3 className="text-greenish font-bold text-2xl py-3">
-          I used to be a Registered Nurse.
+        <h3 className="text-greenish font-bold text-xl py-1 text-center">
+          I passionate about expanding my knowledge in tech field. I can help
+          <br />
+          building user friendly websites using my technical skills, <br />
+          creative thinking, and effective communication.
         </h3>
-        <h3 className="text-greenish font-bold text-xl py-3">
+        <h3 className="text-greenish font-bold text-xl py-1">
           Find more about me{' '}
-          <a
-            href='#about-section'
-            className="text-greenish hover:underline hover:text-night"
-          >
+          <Link to="/about" className="text-night hover:underline">
             here
-          </a>
+          </Link>
           .
         </h3>
       </div>
