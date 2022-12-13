55fbf08f0dcb48ce34abe279014b0abb73b1a758
Sarah Abou Dalal <sarahaboudalal@gmail.com>
2022-12-13 23:37:00 +0200

    typo fix

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -5,10 +5,10 @@ export default function Hero() {
   return (
     <div className="bg-whitish h-full py-4 md:px-0 px-4 font-poppins border-b-2 border-beige dark:bg-greenish dark:border-night dark:border-opacity-20">
       <div className="flex flex-col justify-center items-center py-5">
-        <h3 className="text-greenish font-extrabold md:text-5xl text-3xl py-3 text-center dark:text-beige">
-          Welcom to Sarah's Portfolio
+        <h3 className="text-night font-extrabold md:text-5xl text-3xl py-3 text-center dark:text-whitish">
+          Welcome to Sarah's Portfolio
         </h3>
-        <h3 className="text-greenish font-bold text-2xl py-3 text-center dark:text-beige">
+        <h3 className="text-greenish font-bold md:text-2xl text-xl py-3 text-center dark:text-beige">
           Hi! I'm Sarah Abou Dalal, a
           <BsCode className="md:inline hidden text-greenish w-12 dark:text-beige" />
           <p className="inline italic mx-2 md:mx-0">
@@ -16,7 +16,7 @@ export default function Hero() {
           </p>
           <BsCodeSlash className="md:inline hidden text-greenish w-12 dark:text-beige" />
         </h3>
-        <h3 className="text-greenish font-bold text-xl py-1 text-center dark:text-beige">
+        <h3 className="text-greenish font-bold md:text-xl text-lg py-1 text-center dark:text-beige">
           I'm passionate about programming and development.
           <br /> I can make user friendly and functional websites come to life,{' '}
           <br />
