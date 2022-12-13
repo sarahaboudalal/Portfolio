4ca255084b4181d636ee66939ebd2157e0b6cfdc
Sarah Abou Dalal <sarahaboudalal@gmail.com>
2022-12-13 18:54:56 +0200

    Footer: dark mode added

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,10 +2,10 @@ import { MdEmail } from "react-icons/md";
 
 export default function Footer() {
     return (
-      <div className="bg-whitish border-t-2 border-t-beige py-10 px-2 flex items-center justify-center duration-400">
+      <div className="bg-whitish border-t-2 border-t-beige py-10 px-2 flex items-center justify-center duration-400 dark:bg-greenish dark:border-night dark:border-opacity-20">
         <a
           href="mailto:sarahaboudalal@gmail.com"
-          className="mx-4 text-lg font-medium hover:text-night hover:bg-beige text-greenish border-2 border-greenish px-2 border-dashed hover:border-solid rounded-sm"
+          className="mx-4 text-lg font-medium hover:text-night hover:bg-beige dark:hover:bg-night text-greenish border-2 border-greenish px-2 border-dashed hover:border-solid rounded-sm dark:text-beige dark:hover:text-whitish dark:border-beige"
         >
           Let's Get in Touch <MdEmail className="h-6 pb-1 w-6 inline" />
         </a>
