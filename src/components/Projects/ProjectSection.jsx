46a2540a422df374578ac4c39fd131395fb7da7b
Sarah Abou Dalal <sarahaboudalal@gmail.com>
2022-12-20 19:06:21 +0200

    re-styled projects cards

--- src/components/Projects/ProjectSection.jsx
+++ src/components/Projects/ProjectSection.jsx
@@ -3,11 +3,11 @@ import Projects from './Projects';
 
 export default function ProjectSection() {
   return (
-    <div className="bg-whitish py-14 md:px-2 px-4  dark:bg-greenish">
+    <div className="bg-whitish py-14 md:px-4 px-6  dark:bg-greenish">
       <h3 className="mb-10 text-center overline text-greenish text-4xl font-bold dark:text-beige">
         My Work
       </h3>
-      <div className="grid lg:grid-cols-2 grid-cols-1 content-center justify-evenly justify-items-center gap-y-9 gap-x-3">
+      <div className="grid lg:grid-cols-3 grid-cols-1 content-center justify-evenly justify-items-center gap-y-9 gap-x-5">
         {projects.map((project) => (
           <Projects key={project.id} project={project} />
         ))}
